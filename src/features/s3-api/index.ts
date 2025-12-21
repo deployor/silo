88607e5248152f5f1392c574af681e1cc6389730
Deployor <129990841+deployor@users.noreply.github.com>
2025-12-21 01:15:44 +0100

    E

--- src/features/s3-api/index.ts
+++ src/features/s3-api/index.ts
@@ -120,21 +120,6 @@ export async function handleS3Request(
   const internalPath = getInternalPath(key, user, bucket);
 
   if (key === "") {
-    // Check for forbidden parameters on PUT requests (like ?policy, ?acl)
-    for (const param of forbiddenParams) {
-      if (url.searchParams.has(param)) {
-        return new Response(
-          `<?xml version="1.0" encoding="UTF-8"?>
-<Error>
-  <Code>NotImplemented</Code>
-  <Message>A header you provided implies functionality that is not implemented</Message>
-  <RequestId>0000000000000000</RequestId>
-</Error>`,
-          { status: 501, headers: { "Content-Type": "application/xml" } },
-        );
-      }
-    }
-
     if (method === "PUT") {
       return new Response(
         `
