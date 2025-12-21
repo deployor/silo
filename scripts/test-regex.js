a8bbf4ef8fd7523415ddf0f107d6157d7732711d
Deployor <129990841+deployor@users.noreply.github.com>
2025-12-21 23:32:34 +0100

    EE

--- scripts/test-regex.js
+++
@@ -1,4 +0,0 @@
-
-const path = "/api/dashboard/buckets/my-bucket/keys/123e4567-e89b-12d3-a456-426614174000";
-const match = path.match(/^\/api\/dashboard\/buckets\/([a-z0-9-]+)\/keys\/([^/]+)$/);
-console.log(match);
