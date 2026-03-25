3b2926373e0ad943fc5ce6401c126b063abef267
Deployor <129990841+deployor@users.noreply.github.com>
2026-03-25 10:24:11 +0100

    This is a fun chnge for us

--- src/frontend/components/AppShell.tsx
+++ src/frontend/components/AppShell.tsx
@@ -247,13 +247,6 @@ export function AppShell({
 									Account
 								</a>
 								<a
-									href="/account"
-									className="text-hc-red hover:text-white transition-colors inline-flex items-center gap-2"
-								>
-									<i className="ph ph-trash text-base" />
-									Delete account
-								</a>
-								<a
 									href="/auth/logout"
 									className="text-hc-red hover:text-white transition-colors"
 								>
