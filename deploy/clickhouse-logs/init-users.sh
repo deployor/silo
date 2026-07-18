7e118d2acb9e4dd6b8af64d106fede5c45bbe8b0
Deployor <129990841+deployor@users.noreply.github.com>
2026-07-18 20:28:06 +0200

    fix ClickHouse user bootstrap

--- deploy/clickhouse-logs/init-users.sh
+++ deploy/clickhouse-logs/init-users.sh
@@ -9,7 +9,7 @@ case "${CLICKHOUSE_INGEST_PASSWORD}${CLICKHOUSE_QUERY_PASSWORD}" in
   *"'"*) echo "ClickHouse passwords must not contain single quotes" >&2; exit 1 ;;
 esac
 
-clickhouse client --user silo_admin --password "$CLICKHOUSE_ADMIN_PASSWORD" --multiquery --query "
+clickhouse client --host "${CLICKHOUSE_HOST:-localhost}" --user silo_admin --password "$CLICKHOUSE_ADMIN_PASSWORD" --multiquery --query "
 CREATE USER IF NOT EXISTS silo_ingest IDENTIFIED WITH sha256_password BY '${CLICKHOUSE_INGEST_PASSWORD}';
 ALTER USER silo_ingest IDENTIFIED WITH sha256_password BY '${CLICKHOUSE_INGEST_PASSWORD}';
 GRANT INSERT ON silo_logs.request_logs TO silo_ingest;
